43dc1f13328dea23a919d2805b09cb2cd146aff2
b00nedawgy <brendanrboone@gmail.com>
2024-09-25 20:19:56 -0700

    hitbox progress. need to fix hit boxes to character

--- hitbox.lua
+++ hitbox.lua
@@ -3,18 +3,26 @@ Hitbox.__index = Hitbox
 
 ActiveHitboxes = {}
 
-function Hitbox.new(x, y, width, height, damage)
+function Hitbox.new(srcBody, xOff, yOff, width, height, damage)
     local instance = setmetatable({}, Hitbox)
 
-    instance.x = x
-    instance.y = y
+    instance.srcBody = {} -- associated body "attached"
+    instance.srcBody.body = srcBody
+    instance.srcBody.x = srcBody:getX()
+    instance.srcBody.y = srcBody:getY()
+
+    instance.xOff = xOff -- offset from associated body
+    instance.yOff = yOff
     instance.width = width
     instance.height = height
+
+    instance.xPos = instance.assbod.x + instance.xOff -- hitbox x coordinate
+    instance.yPos = instance.assbod.y + instance.yOff
 
     instance.damage = damage
 
     instance.physics = {}
-    instance.physics.body = love.physics.newBody(World, instance.x, instance.y, "kinematic")
+    instance.physics.body = love.physics.newBody(World, instance.xPos, instance.yPos, "kinematic")
     instance.physics.shape = love.physics.newRectangleShape(instance.width, instance.height)
     instance.physics.fixture = love.physics.newFixture(instance.physics.body, instance.physics.shape)
     instance.physics.fixture:setSensor(true)
@@ -26,13 +34,17 @@ function Hitbox:update(dt)
 
 end
 
+function Hitbox:syncAssociate()
+
+end
+
 function Hitbox:draw()
 
 end
 
-function Hitbox.Escape(key)
-    if key == "escape" then
-        love.event.quit()
+function Hitbox.updateAll(dt)
+    for _, instance in ipairs(ActiveHitboxes) do
+        instance:update(dt)
     end
 end
 
